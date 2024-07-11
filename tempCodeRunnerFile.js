6127f66636e8b9e71b1a7f5382e7c6c60c6f5634
Bhimashankar <bhimd2446@gmail.com>
2024-07-11 19:51:33 +0530

    my js mini projects

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,21 @@
+function wordToNumberAverage(inputString) {
+//     let sum = 0;
+//     for (let i = 0; i < inputString.length; i++) {
+//       // Get the ASCII value of the character and subtract 96 to get its position in the alphabet (a=1, b=2, ..., z=26)
+//       let charValue = inputString.charCodeAt(i) - 96;
+//       sum += charValue;
+//       console.log(charValue);
+//     }
+    
+//     // Calculate the average
+//     let average = sum / inputString.length;
+//     console.log(`Input string: ${inputString}`);
+//   console.log(`Average of character values: ${average}`);
+//     return 0;
+//   }
+  
+//   // Example usage:
+//   let input = 'abcd';
+// wordToNumberAverage(input);
+// wordToNumberAverage("pink");
+// wordToNumberAverage("red");
